82d25dd752ac1f0c2fd18ec6fd794c927b15442c
Bryant Facenda <bryantffacen@gmail.com>
2026-07-21 23:12:18 -0400

    Correciones en suggested: -Implementacion de packing: cantida de empaque. -Actualizacion de returno de la misma funcion. Rediseno de index.html, para vista moviles mas comoda. integracion de nuevos columnas al archivo de procesamiento: UXE, S

--- formula.py
+++ formula.py
@@ -1,6 +1,12 @@
+import math
+
 def suggested(
-    average_sale: int, replenishment_time: int, current_stock: int, safety_stock: int
-) -> int:
+    average_sale: int,
+    replenishment_time: int,
+    current_stock: int,
+    safety_stock: int,
+    packing: int,
+) -> dict:
     """
     Calcula la cantidad sugerida de compra para la orden de compra (OC).
 
@@ -9,14 +15,17 @@ def suggested(
         replenishment_time (int): Tiempo que tarda el proveedor en entregar (en días o meses).
         current_stock (int): Inventario físico actual en el almacén.
         safety_stock (int): Stock de colchón para prevenir desabastecimiento.
+        packing (int): Cantida viene en el empaque.
 
     Returns:
-        int: Cantidad óptima a solicitar (devuelve 0 si hay sobrestock o faltan datos).
+        dict: Un diccionario con:
+        - "cantidad" (int): Cantidad óptima en unidades a solicitar.
+        - "empaque" (int): Cantidad de cajas/paquetes cerrados para el proveedor.
     """
 
     if average_sale is None or replenishment_time is None or current_stock is None:
-        print("Sin datos para relizar el calculo")
-        return 0
+        print("Sin datos para realizar el calculo")
+        return {"cantidad": 0, "empaque": 0}
 
     optimal_inventory = average_sale * replenishment_time + safety_stock
 
@@ -25,8 +34,9 @@ def suggested(
     if result < 0:
         return 0
 
-    print(result)
-    return round(result)
+    necessary_packages = math.ceil(result / packing)
+
+    return {"cantidad": necessary_packages * packing, "empaque": necessary_packages}
 
 
 def average_weekly_sales(sales_week: list) -> int:
@@ -43,9 +53,9 @@ def average_weekly_sales(sales_week: list) -> int:
         ValueError: Si la lista `sales_week` está vacía.
     """
     print(f"Procesando el promedio de ventas {sales_week}")
-    
+
     if not sales_week:
         print("Ventas vacias")
         raise ValueError("La lista de ventas no puede eatar vacía.")
-    
-    return int(sum(sales_week)/len(sales_week))
+
+    return int(sum(sales_week) / len(sales_week))
